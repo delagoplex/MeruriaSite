f770991dfa1295849b04b9469456e8e3a4054bae
Pascal Eberhard <pascal.eberhard@gmx.net>
2026-05-06 17:45:31 +0200

    configured racecards to look like godcards without domains added configurable insigniaSrc to racecards

--- assets/scripts/data/rassen-data.js
+++ assets/scripts/data/rassen-data.js
@@ -60,69 +60,71 @@ window.RACE_PAGES = {
   'huegelzwerge':    null,
 };
 
+const _insignia = 'assets/images/gods/insignia/Insignie_Aurelia.png';
+
 window.RASSEN_DATA = [
-  { type:'race',  id:'aarakocra',      name:'Aarakocra' },
-  { type:'race',  id:'aasimar',        name:'Aasimar' },
-  { type:'race',  id:'dhampire',       name:'Dhampire' },
+  { type:'race',  id:'aarakocra',      name:'Aarakocra',      insigniaSrc: _insignia },
+  { type:'race',  id:'aasimar',        name:'Aasimar',        insigniaSrc: _insignia },
+  { type:'race',  id:'dhampire',       name:'Dhampire',       insigniaSrc: _insignia },
   { type:'group', id:'drachenbluetige', name:'Drachenblütige', subraces:[
-    { id:'chromatische', name:'Chromatische Drachenblütige' },
-    { id:'edelstein',    name:'Edelstein Drachenblütige' },
-    { id:'metallische',  name:'Metallische Drachenblütige' }
+    { id:'chromatische', name:'Chromatische Drachenblütige', insigniaSrc: _insignia },
+    { id:'edelstein',    name:'Edelstein Drachenblütige',    insigniaSrc: _insignia },
+    { id:'metallische',  name:'Metallische Drachenblütige',  insigniaSrc: _insignia }
   ]},
-  { type:'race',  id:'echsenmenschen', name:'Echsenmenschen' },
+  { type:'race',  id:'echsenmenschen', name:'Echsenmenschen', insigniaSrc: _insignia },
   { type:'group', id:'elfen',          name:'Elfen', subraces:[
-    { id:'dunkelelfen',  name:'Dunkelelfen' },
-    { id:'eladrin',      name:'Eladrin' },
-    { id:'hochelfen',    name:'Hochelfen' },
-    { id:'meereselfen',  name:'Meereselfen' },
-    { id:'schattenfeen', name:'Schattenfeen' },
-    { id:'waldelfen',    name:'Waldelfen' }
+    { id:'dunkelelfen',  name:'Dunkelelfen',  insigniaSrc: _insignia },
+    { id:'eladrin',      name:'Eladrin',      insigniaSrc: _insignia },
+    { id:'hochelfen',    name:'Hochelfen',    insigniaSrc: _insignia },
+    { id:'meereselfen',  name:'Meereselfen',  insigniaSrc: _insignia },
+    { id:'schattenfeen', name:'Schattenfeen', insigniaSrc: _insignia },
+    { id:'waldelfen',    name:'Waldelfen',    insigniaSrc: _insignia }
   ]},
-  { type:'race',  id:'feen',           name:'Feen' },
-  { type:'race',  id:'firbolg',        name:'Firbolg' },
+  { type:'race',  id:'feen',           name:'Feen',           insigniaSrc: _insignia },
+  { type:'race',  id:'firbolg',        name:'Firbolg',        insigniaSrc: _insignia },
   { type:'group', id:'genasi',         name:'Genasi', subraces:[
-    { id:'erd-genasi',   name:'Erd-Genasi' },
-    { id:'feuer-genasi', name:'Feuer-Genasi' },
-    { id:'luft-genasi',  name:'Luft-Genasi' },
-    { id:'wasser-genasi',name:'Wasser-Genasi' }
+    { id:'erd-genasi',    name:'Erd-Genasi',    insigniaSrc: _insignia },
+    { id:'feuer-genasi',  name:'Feuer-Genasi',  insigniaSrc: _insignia },
+    { id:'luft-genasi',   name:'Luft-Genasi',   insigniaSrc: _insignia },
+    { id:'wasser-genasi', name:'Wasser-Genasi', insigniaSrc: _insignia }
   ]},
-  { type:'race',  id:'githyanki',      name:'Githyanki' },
-  { type:'race',  id:'githzerai',      name:'Githzerai' },
+  { type:'race',  id:'githyanki',      name:'Githyanki',      insigniaSrc: _insignia },
+  { type:'race',  id:'githzerai',      name:'Githzerai',      insigniaSrc: _insignia },
   { type:'group', id:'gnome',          name:'Gnome', subraces:[
-    { id:'felsengnome',  name:'Felsengnome' },
-    { id:'tiefengnome',  name:'Tiefengnome' },
-    { id:'waldgnome',    name:'Waldgnome' }
+    { id:'felsengnome',  name:'Felsengnome',  insigniaSrc: _insignia },
+    { id:'tiefengnome',  name:'Tiefengnome',  insigniaSrc: _insignia },
+    { id:'waldgnome',    name:'Waldgnome',    insigniaSrc: _insignia }
   ]},
-  { type:'race',  id:'goblins',        name:'Goblins' },
-  { type:'race',  id:'goliaths',       name:'Goliaths' },
-  { type:'race',  id:'grottenschrate', name:'Grottenschrate' },
-  { type:'race',  id:'halbelfen',      name:'Halbelfen' },
+  { type:'race',  id:'goblins',        name:'Goblins',        insigniaSrc: _insignia },
+  { type:'race',  id:'goliaths',       name:'Goliaths',       insigniaSrc: _insignia },
+  { type:'race',  id:'grottenschrate', name:'Grottenschrate', insigniaSrc: _insignia },
+  { type:'race',  id:'halbelfen',      name:'Halbelfen',      insigniaSrc: _insignia },
   { type:'group', id:'halblinge',      name:'Halblinge', subraces:[
-    { id:'leichtfuesse', name:'Leichtfüße' },
-    { id:'staemmige',    name:'Stämmige' }
+    { id:'leichtfuesse', name:'Leichtfüße', insigniaSrc: _insignia },
+    { id:'staemmige',    name:'Stämmige',   insigniaSrc: _insignia }
   ]},
-  { type:'race',  id:'halborks',       name:'Halborks' },
-  { type:'race',  id:'harengons',      name:'Harengons' },
-  { type:'race',  id:'hexblute',       name:'Hexblute' },
-  { type:'race',  id:'hobgoblins',     name:'Hobgoblins' },
-  { type:'race',  id:'kenku',          name:'Kenku' },
-  { type:'race',  id:'kobolde',        name:'Kobolde' },
-  { type:'race',  id:'menschen',       name:'Menschen' },
-  { type:'race',  id:'minotauren',     name:'Minotauren' },
-  { type:'race',  id:'orks',           name:'Orks' },
-  { type:'race',  id:'satyrn',         name:'Satyrn' },
-  { type:'race',  id:'tabaxi',         name:'Tabaxi' },
-  { type:'race',  id:'tieflinge',      name:'Tieflinge' },
-  { type:'race',  id:'tortels',        name:'Tortels' },
-  { type:'race',  id:'tritons',        name:'Tritons' },
-  { type:'race',  id:'wandler',        name:'Wandler' },
-  { type:'race',  id:'wechselbälger',  name:'Wechselbälger' },
-  { type:'race',  id:'wiedergeborene', name:'Wiedergeborene' },
-  { type:'race',  id:'yuan-ti',        name:'Yuan-ti' },
-  { type:'race',  id:'zentauren',      name:'Zentauren' },
+  { type:'race',  id:'halborks',       name:'Halborks',       insigniaSrc: _insignia },
+  { type:'race',  id:'harengons',      name:'Harengons',      insigniaSrc: _insignia },
+  { type:'race',  id:'hexblute',       name:'Hexblute',       insigniaSrc: _insignia },
+  { type:'race',  id:'hobgoblins',     name:'Hobgoblins',     insigniaSrc: _insignia },
+  { type:'race',  id:'kenku',          name:'Kenku',          insigniaSrc: _insignia },
+  { type:'race',  id:'kobolde',        name:'Kobolde',        insigniaSrc: _insignia },
+  { type:'race',  id:'menschen',       name:'Menschen',       insigniaSrc: _insignia },
+  { type:'race',  id:'minotauren',     name:'Minotauren',     insigniaSrc: _insignia },
+  { type:'race',  id:'orks',           name:'Orks',           insigniaSrc: _insignia },
+  { type:'race',  id:'satyrn',         name:'Satyrn',         insigniaSrc: _insignia },
+  { type:'race',  id:'tabaxi',         name:'Tabaxi',         insigniaSrc: _insignia },
+  { type:'race',  id:'tieflinge',      name:'Tieflinge',      insigniaSrc: _insignia },
+  { type:'race',  id:'tortels',        name:'Tortels',        insigniaSrc: _insignia },
+  { type:'race',  id:'tritons',        name:'Tritons',        insigniaSrc: _insignia },
+  { type:'race',  id:'wandler',        name:'Wandler',        insigniaSrc: _insignia },
+  { type:'race',  id:'wechselbälger',  name:'Wechselbälger',  insigniaSrc: _insignia },
+  { type:'race',  id:'wiedergeborene', name:'Wiedergeborene', insigniaSrc: _insignia },
+  { type:'race',  id:'yuan-ti',        name:'Yuan-ti',        insigniaSrc: _insignia },
+  { type:'race',  id:'zentauren',      name:'Zentauren',      insigniaSrc: _insignia },
   { type:'group', id:'zwerge',         name:'Zwerge', subraces:[
-    { id:'gebirgszwerge',name:'Gebirgszwerge' },
-    { id:'grauzwerge',   name:'Grauzwerge' },
-    { id:'huegelzwerge', name:'Hügelzwerge' }
+    { id:'gebirgszwerge', name:'Gebirgszwerge', insigniaSrc: _insignia },
+    { id:'grauzwerge',    name:'Grauzwerge',    insigniaSrc: _insignia },
+    { id:'huegelzwerge',  name:'Hügelzwerge',   insigniaSrc: _insignia }
   ]}
 ];
